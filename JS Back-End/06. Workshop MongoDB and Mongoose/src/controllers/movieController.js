c5608174d32a1d84c35d860fc45b8f0918de6262
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-01-27 21:24:26 +0200

    Add mongoose, use mongoose for movie create

---
+++ JS Back-End/06. Workshop MongoDB and Mongoose/src/controllers/movieController.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const movieService = require('../services/movieService');
+
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+router.post('/create', async (req, res) => {
+    const newMovie = req.body;
+
+    try {
+        await movieService.create(newMovie);
+    }
+    catch (err) {
+        console.log(err.message);
+        res.redirect('/create');
+    }
+
+    res.redirect('/');
+});
+
+router.get('/details/:movieId', (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = movieService.getOne(movieId);
+
+    movie.ratingArr = new Array(Number(movie.rating)).fill(true);
+
+    res.render('details', movie);
+
+});
+
+module.exports = router;
