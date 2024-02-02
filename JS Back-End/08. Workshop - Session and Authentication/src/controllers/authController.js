c6858c7ee3b650b5d695ae2ab4e8f8f42e217e96
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-02 13:30:19 +0200

    Show register view, add registered user to db

---
+++ JS Back-End/08. Workshop - Session and Authentication/src/controllers/authController.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const userService = require('../services/authService');
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    await authService.register(userData);
+
+    res.redirect('/auth/login');
+});
+
+module.exports = router;
