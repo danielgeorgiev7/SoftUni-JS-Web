e8450a79a3cff57e62ac51c28145e417936f87a8
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-08 23:51:16 +0200

    Add item model

---
+++ JS Back-End/10. Exam prep - Oct - 2023/src/models/Item.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    damages: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    production: {
+        type: Number,
+        required: true,
+    },
+    exploitation: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    buyingList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
