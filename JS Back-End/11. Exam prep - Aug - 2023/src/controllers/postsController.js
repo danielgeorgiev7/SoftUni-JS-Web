f22d0e1f0ce2caa6df3515c3968b111dba571153
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-12 16:35:52 +0200

    Added details, vote, edit and delete

--- JS Back-End/11. Exam prep - Aug - 2023/src/controllers/postsController.js
+++ JS Back-End/11. Exam prep - Aug - 2023/src/controllers/postsController.js
@@ -17,14 +17,82 @@ router.post('/create', async (req, res) => {
         owner: req.user._id
     };
 
-    console.log(req.body);
     try {
         await postsService.create(data);
         res.redirect('/posts/all');
     }
     catch (err) {
         const message = getErrorMessage(err);
-        res.render('posts/create', { error: message, ...req.body })
+        res.status(400).render('posts/create', { error: message, ...req.body })
+    }
+});
+
+router.get('/details/:postId', async (req, res) => {
+    try {
+        const creature = await postsService
+            .getOne(req.params.postId,
+                { path: 'votes', select: 'email' },
+                { path: 'owner', select: 'firstName lastName' }
+            );
+        const isOwner = creature.owner._id == req.user._id;
+        const ownerFullname = `${creature.owner.firstName} ${creature.owner.lastName}`
+        const hasVoted = creature.votes.filter((voter) => voter._id == req.user._id);
+        const votesCount = creature.votes.length;
+        const votersEmails = [];
+        creature.votes.map(voter => votersEmails.push(voter.email));
+        votersEmails.join(', ');
+        res.render('posts/details', { ...creature, isOwner, ownerFullname, hasVoted, votesCount, votersEmails });
+    }
+    catch (err) {
+        //TODO render to /all when image validation is added
+    }
+});
+
+router.get('/edit/:postId', async (req, res) => {
+    const creature = await postsService.getOne(req.params.postId);
+    try {
+        res.render('posts/edit', creature);
+    }
+    catch (err) {
+        const message = getErrorMessage(err);
+        res.status(400).render('posts/all', { error: message, ...creature });
+    }
+});
+
+router.post('/edit/:postId', async (req, res) => {
+    try {
+        await postsService.updateOne(req.params.postId, req.body);
+        res.redirect(`/posts/details/${req.params.postId}`);
+    }
+    catch (err) {
+        const message = getErrorMessage(err);
+        res.status(400).render('posts/edit', { error: message, ...req.body });
+    }
+});
+
+router.get('/vote/:postId', async (req, res) => {
+    try {
+        const creature = await postsService.updateOne(req.params.postId, { $push: { votes: req.user._id } })
+        res.redirect(`/posts/details/${req.params.postId}`);
+    }
+    catch (err) {
+        const message = getErrorMessage(err);
+        res.status(400).render('posts/details', { error: message, ...creature });
+    }
+});
+
+
+router.get('/delete/:postId', async (req, res) => {
+    try {
+        const post = await postsService.getOne(req.params.postId);
+        if (post.owner._id != req.user._id) throw new Error('Unauthorized');
+
+        await postsService.deleteOne(req.params.postId)
+            .then(res.redirect('/posts/all'));
+    }
+    catch (err) {
+        const message = getErrorMessage(err);
+        res.status(400).render('home', { error: message });
     }
 });
 
--- JS Back-End/11. Exam prep - Aug - 2023/src/services/postsService.js
+++ JS Back-End/11. Exam prep - Aug - 2023/src/services/postsService.js
@@ -1,5 +1,11 @@
 const Creature = require("../models/Creatures");
 
+exports.create = async (data) => await Creature.create(data);
+
 exports.getAll = async () => await Creature.find().lean();
 
-exports.create = async (data) => await Creature.create(data);
+exports.getOne = async (postId, popOptions1 = '', popOptions2 = '') => await Creature.findById(postId).populate(popOptions1).populate(popOptions2).lean();
+
+exports.updateOne = async (postId, data) => await Creature.findByIdAndUpdate(postId, data).lean();
+
+exports.deleteOne = async (postId) => await Creature.findByIdAndDelete(postId).lean();
