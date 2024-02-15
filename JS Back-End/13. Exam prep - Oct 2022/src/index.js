299aa2f16fd0e471370e82857c39a86ae5505eff
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-15 05:43:56 +0200

    Add starter files, rework html and add base files

---
+++ JS Back-End/13. Exam prep - Oct 2022/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const configHandlebars = require('./config/configHandlebars');
+const configExpress = require('./config/configExpress');
+
+const app = express();
+const port = 3000;
+
+configExpress(app);
+configHandlebars(app);
+
+app.use(routes);
+
+mongoose.connect(`mongodb://localhost:27017/friendly-world`)
+    .then(() => {
+        console.log('DB connection established');
+
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    })
+    .catch(err => console.error(err.message));
