3c2b293d704408c8e27567588fc1f8d2991e81fe
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-16 21:40:16 +0200

    Add structure and starter files

---
+++ JS Back-End/13. Exam prep - Jun - 2022/src/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//TODO Change parameters
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 12);
+});
+
+// userSchema.virtual('rePassword')
+//     .set(function (value) {
+//         if (value !== this.password) {
+//             throw new Error('Passwords does not match');
+//         }
+//     });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ JS Back-End/13. Exam prep - Jun - 2022/src/utils/errorUtils.js
@@ -0,0 +1,12 @@
+const { MongooseError } = require('mongoose');
+
+exports.getErrorMessage = (err) => {
+    let message = '';
+    if (err instanceof MongooseError) {
+        message = Object.values(err.errors).at(0).message
+    }
+    else if (err instanceof Error) {
+        err.message = message;
+    }
+    return message;
+};
