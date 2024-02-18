836ca8d737a6ebc8902dfe8fc8281eeec4c2b01f
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-18 14:43:45 +0200

    Add create and dynamic dashboard

--- JS Back-End/15. Final Exam/src/controllers/postsController.js
+++ JS Back-End/15. Final Exam/src/controllers/postsController.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+const postsService = require('../services/postsService');
+const { getErrorMessage } = require('../utils/errorUtils');
 
-router.get('/dashboard', (req, res) => {
-    res.render('posts/dashboard');
+
+router.get('/dashboard', async (req, res) => {
+    const posts = await postsService.getAll().lean();
+    res.render('posts/dashboard', { posts });
+});
+
+router.get('/create', (req, res) => {
+    res.render('posts/create');
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        const data = {
+            owner: req.user._id,
+            ...req.body
+        };
+        await postsService.create(data);
+        res.redirect('/posts/dashboard');
+    } catch (err) {
+        console.log(err);
+        const message = getErrorMessage(err);
+        res.render('posts/create', { error: message, ...req.body });
+    }
 });
 
 router.get('/details/:postId', (req, res) => {
